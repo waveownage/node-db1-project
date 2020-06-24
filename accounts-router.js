aa45b730933545355397063feb6ec0af88b5a6f4
Andre Freeman <asf@mail.usf.edu>
2020-06-23 23:22:17 -0400

    Created PUT and DELETE Endpoints

--- accounts-router.js
+++ accounts-router.js
@@ -1,47 +1,80 @@
-const express = require("express")
-const db = require("./data/dbConfig")
+const express = require("express");
+const db = require("./data/dbConfig");
 
-const router = express.Router()
+const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-	try {
-		const accounts = await db.select("*").from("accounts")
+  try {
+    const accounts = await db.select("*").from("accounts");
 
-		res.json(accounts)
-	} catch (err) {
-		next(err)
-	}
-})
+    res.json(accounts);
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.get("/:id", async (req, res, next) => {
-	try {
-		const [account] = await db
-			.select("*")
-			.from("accounts")
-			.where("id", req.params.id)
-			.limit(1)
+  try {
+    const [account] = await db
+      .select("*")
+      .from("accounts")
+      .where("id", req.params.id)
+      .limit(1);
 
-		res.json(account)
-	} catch (err) {
-		next(err)
-	}
-})
+    res.json(account);
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.post("/", async (req, res, next) => {
-	try {
-		const payload = {
-			name: req.body.name,
-			budget: req.body.budget,
-		}
-		const [accountID] = await db.insert(payload).into("accounts")
+  try {
+    const payload = {
+      name: req.body.name,
+      budget: req.body.budget
+    };
+    const [accountID] = await db.insert(payload).into("accounts");
 
-		const account = await db.first("*").from("accounts").where("id", accountID)
+    const account = await db
+      .first("*")
+      .from("accounts")
+      .where("id", accountID);
 
-		res.status(201).json(account)
-	} catch (err) {
-		next(err)
-	}
-})
+    res.status(201).json(account);
+  } catch (err) {
+    next(err);
+  }
+});
 
+router.put("/:id", async (req, res, next) => {
+  try {
+    const payload = {
+      name: req.body.name,
+      budget: req.body.budget
+    };
+    await db("accounts")
+      .update(payload)
+      .where("id", req.params.id);
+    const account = await db
+      .first("*")
+      .from("accounts")
+      .where("id", req.params.id);
 
-module.exports = router
+    res.json(account);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    await db("accounts")
+      .where("id", req.params.id)
+      .del();
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
