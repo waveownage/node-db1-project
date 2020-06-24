e328643a687e3bef6825588cf030468b760e6b86
Andre Freeman <asf@mail.usf.edu>
2020-06-23 23:08:22 -0400

    Created Get By ID Endpoint

--- accounts-router.js
+++ accounts-router.js
@@ -5,13 +5,27 @@ const router = express.Router()
 
 router.get("/", async (req, res, next) => {
 	try {
-		// translates to `SELECT * FROM "messages";`
-		const names = await db.select("*").from("accounts")
+		const accounts = await db.select("*").from("accounts")
 
-		res.json(names)
+		res.json(accounts)
 	} catch (err) {
 		next(err)
 	}
 })
+
+router.get("/:id", async (req, res, next) => {
+	try {
+		const [account] = await db
+			.select("*")
+			.from("accounts")
+			.where("id", req.params.id)
+			.limit(1)
+
+		res.json(account)
+	} catch (err) {
+		next(err)
+	}
+})
+
 
 module.exports = router
