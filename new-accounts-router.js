58736209ba8e8faed7e0f2eed821b43a8e5fafae
Andre Freeman <asf@mail.usf.edu>
2020-08-19 23:02:17 -0400

    Stretch Queries

--- new-accounts-router.js
+++ new-accounts-router.js
@@ -6,72 +6,85 @@ const router = express.Router();
 //GET all accounts
 
 router.get("/", async (req, res, next) => {
-    try {
-      const accounts = await db.select("*").from("accounts");
-  
-      res.json(accounts);
-    } catch (err) {
-      next(err);
-    }
-  });
+  try {
+    const accounts = await db.select("*").from("accounts");
+
+    res.json(accounts);
+  } catch (err) {
+    next(err);
+  }
+});
 
 //GET account by ID
 
 router.get("/:id", async (req, res, next) => {
-    try {
-      const [account] = await db
-        .select("*")
-        .from("accounts")
-        .where("id", req.params.id)
-        .limit(1);
-  
-      res.json(account);
-    } catch (err) {
-      next(err);
-    }
-  });
+  try {
+    const [account] = await db
+      .select("*")
+      .from("accounts")
+      .where("id", req.params.id)
+      .limit(1);
+
+    res.json(account);
+  } catch (err) {
+    next(err);
+  }
+});
 
 //Add account
 
 router.post("/", async (req, res, next) => {
-    try {
-        const [id] = await db.insert({
-            name: req.body.name,
-            budget: req.body.budget,
-        })
-        .into("accounts")
+  try {
+    const [id] = await db
+      .insert({
+        name: req.body.name,
+        budget: req.body.budget
+      })
+      .into("accounts");
 
-        const account = await db("accounts")
-        .where("id", id)
-        .first()
+    const account = await db("accounts")
+      .where("id", id)
+      .first();
 
-        res.status(201).json(account)
-    } catch(err) {
-        next(err)
-    }
-})
+    res.status(201).json(account);
+  } catch (err) {
+    next(err);
+  }
+});
 
 //Update Account
 
 router.put("/:id", async (req, res, next) => {
-    try {
-      const payload = {
-        name: req.body.name,
-        budget: req.body.budget
-      };
-      await db("accounts")
-        .update(payload)
-        .where("id", req.params.id);
-      const account = await db
-        .first("*")
-        .from("accounts")
-        .where("id", req.params.id);
-  
-      res.json(account);
-    } catch (err) {
-      next(err);
-    }
-  });
+  try {
+    const payload = {
+      name: req.body.name,
+      budget: req.body.budget
+    };
+    await db("accounts")
+      .update(payload)
+      .where("id", req.params.id);
+    const account = await db
+      .first("*")
+      .from("accounts")
+      .where("id", req.params.id);
+
+    res.json(account);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//Delete Account
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    await db("accounts")
+      .where("id", req.params.id)
+      .del();
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
-
--- queries.sql
+++ queries.sql
@@ -41,4 +41,11 @@ where CustomerName = 'The Shire';
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
+SELECT DISTINCT City
+FROM [Customers]
+
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+
+SELECT LENGTH(SupplierName)
+FROM [Suppliers]
+WHERE LENGTH(SupplierName) > 20
