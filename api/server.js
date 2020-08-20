623961ff633fe1309dc9e3a9c2a66279871a9721
Andre Freeman <asf@mail.usf.edu>
2020-08-19 20:06:21 -0400

    Restart Assignment

--- api/server.js
+++ api/server.js
@@ -1,11 +1,9 @@
 const express = require("express");
 
 const db = require("../data/dbConfig.js");
-const router = require("../accounts-router");
 
 const server = express();
 
 server.use(express.json());
-server.use(router);
 
 module.exports = server;
