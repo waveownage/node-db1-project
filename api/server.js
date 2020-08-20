85ac2ed772f283c1b5c9912c468132717cbc0bea
Andre Freeman <asf@mail.usf.edu>
2020-08-19 20:37:24 -0400

    Created get all accounts endpoint

--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 
 const db = require("../data/dbConfig.js");
+const router = require("../new-accounts-router")
 
 const server = express();
+server.use(router);
 
 server.use(express.json());
 
--- new-accounts-router.js
+++ new-accounts-router.js
@@ -3,5 +3,17 @@ const db = require("./data/dbConfig");
 
 const router = express.Router();
 
+//GET all accounts
+
+router.get("/", async (req, res, next) => {
+    try {
+      const accounts = await db.select("*").from("accounts");
+  
+      res.json(accounts);
+    } catch (err) {
+      next(err);
+    }
+  });
+
 module.exports = router;
 
